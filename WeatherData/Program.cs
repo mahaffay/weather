6ac7b614d849d466768e22e61b7dd56b3aceb504
jmahaffay <justin.mahaffay@yahoo.com>
2023-11-03 13:52:30 -0500

    Add project files.

---
+++ WeatherData/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace WeatherData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                @"weather.dat");
+
+            List<Temps> tempList = new List<Temps>();
+            string[] files = File.ReadAllLines(path);
+            
+            for (int m = 2; m < files.Length - 1; m++)
+            {
+                string[] row = files[m].Replace("*", "").Trim().Split(' ');
+                row = row.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                tempList.Add(new Temps()
+                {
+                    day = Convert.ToInt16(row[0]),
+                    maxTemp = Convert.ToInt16(row[1]),
+                    minTemp = Convert.ToInt16(row[2]), 
+                    difference = Convert.ToInt16(row[1]) - Convert.ToInt16(row[2])
+                });
+            }
+
+            var result = tempList.OrderBy(a => a.difference);
+
+            var smallestDiff = result.Select(x => x.day).First();
+
+            Console.WriteLine("June " + smallestDiff.ToString() + " had the smallest temperature change in 2002");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WeatherData/Temps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherData
+{
+    class Temps
+    {
+        public int day { get; set; }
+        public int minTemp { get; set; }
+        public int maxTemp { get; set; }
+        public int difference { get; set; }
+
+    }
+}
